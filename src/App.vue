321f782e901107595fb5ba9d8ed057d4c9425eec
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-08-14 08:14:39 +0200

    Add global styling

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,18 @@ export default defineComponent({
 $btn-color: #31b73a;
 body {
 	background-color: #eefefe;
+	font-family: "Exo 2", sans-serif, monospace;
 	#app {
 		width: 90%;
 		margin: 0 auto;
 	}
 	ul {
 		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	a {
+		text-decoration: none;
 	}
 }
 </style>
